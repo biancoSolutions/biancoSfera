14727e233606112c2cd5b3c45b50203fc2a32b8d
SlothSky <implimail@gmail.com>
2023-03-30 19:08:31 +0200

    Add working version of STM32 SPI PoC repository w/ LCD init

---
+++ 02_SW/001_poc/001_STM32_SPI_PoC/STM32CubeIDE/Debug/Application/Startup/startup_stm32u5a5zjtxq.d
@@ -0,0 +1,2 @@
+Application/Startup/startup_stm32u5a5zjtxq.o: \
+ ../Application/Startup/startup_stm32u5a5zjtxq.s
--- 02_SW/SPI_PoC/lcd.h
+++
@@ -1,9 +0,0 @@
-/*
- * lcd.h
- *
- * Created: 14.03.2023 21:39:25
- *  Author: Jan.WEISS
- */ 
-
-void LCD_Init(void);
-
