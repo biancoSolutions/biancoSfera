e59f28ba39346ee4a9664d0e08cae0334dd944af
SlothSky <implimail@gmail.com>
2023-03-30 21:19:53 +0200

    Create defines for non ASCII LCD characters

--- 02_SW/001_poc/000_M0plus_SPI_PoC/lcd.h
+++ 02_SW/001_poc/000_M0plus_SPI_PoC/lcd.h
@@ -5,7 +5,25 @@
  *  Author: Jan.WEISS
  */ 
 
-void LCD_Init(void);
+#define LCD_ARROW_RIGHT     0x7E
+#define LCD_ARROW_LEFT      0x7F
+#define LCD_UNFILLED_SQUARE 0xDB
+#define LCD_ALPHA           0xE0
+#define LCD_AE              0xE1
+#define LCD_BETA            0xE2
+#define LCD_ETA             0xE3
+#define LCD_MY              0xE4
+#define LCD_DELTA           0xE5
+#define LCD_OE              0xEF
+#define LCD_SIGMA           0xF2
+#define LCD_INFINITY        0xF3
+#define LCD_OMEGA           0xF4
+#define LCD_UE              0xF5
+#define LCD_PI              0xF7
+#define LCD_SPACE_INVADER   0xFC
+#define LCD_BLANK_SQUARE    0xFE
+#define LCD_FILLED_SQUARE   0xFF
 
+void LCD_Init(void);
 
 
