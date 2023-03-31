b1229825e9b60e7cf58950a1991f5785e078e1d6
Jan <implimail@gmail.com>
2023-03-31 18:30:58 +0200

    Integrate pull request review input

--- 02_SW/001_poc/000_M0plus_SPI_PoC/lcd.h
+++ 02_SW/001_poc/000_M0plus_SPI_PoC/lcd.h
@@ -5,32 +5,44 @@
  *  Author: Jan.WEISS
  */ 
 
-// DEFINE NON-ASCII CHARACTERS
-#define LCD_ARROW_RIGHT     0x7E
-#define LCD_ARROW_LEFT      0x7F
-#define LCD_UNFILLED_SQUARE 0xDB
-#define LCD_ALPHA           0xE0
-#define LCD_AE              0xE1
-#define LCD_BETA            0xE2
-#define LCD_ETA             0xE3
-#define LCD_MY              0xE4
-#define LCD_DELTA           0xE5
-#define LCD_OE              0xEF
-#define LCD_SIGMA           0xF2
-#define LCD_INFINITY        0xF3
-#define LCD_OMEGA           0xF4
-#define LCD_UE              0xF5
-#define LCD_PI              0xF7
-#define LCD_SPACE_INVADER   0xFC
-#define LCD_BLANK_SQUARE    0xFE
-#define LCD_FILLED_SQUARE   0xFF
+// NON-ASCII CHARACTERS (only valid with RS == 1)
+typedef enum 
+{
+    LCD_ARROW_RIGHT =     (0x7Eu),
+    LCD_ARROW_LEFT =      (0x7Fu),
+    LCD_UNFILLED_SQUARE = (0xDBu),
+    LCD_ALPHA =           (0xE0u),
+    LCD_AE =              (0xE1u),
+    LCD_BETA =            (0xE2u),
+    LCD_ETA =             (0xE3u),
+    LCD_MY =              (0xE4u),
+    LCD_DELTA =           (0xE5u),
+    LCD_OE =              (0xEFu),
+    LCD_SIGMA =           (0xF2u),
+    LCD_INFINITY =        (0xF3u),
+    LCD_OMEGA =           (0xF4u),
+    LCD_UE =              (0xF5u),
+    LCD_PI =              (0xF7u),
+    LCD_SPACE_INVADER =   (0xFCu),
+    LCD_BLANK_SQUARE =    (0xFEu),
+    LCD_FILLED_SQUARE =   (0xFFu),
+} LCD_NonAsciiChars_t;
 
-// DEFINE LCD CONTROL CHARACTERS
-#define CLEAR_DISPLAY       0x01
-#define RETURN_HOME         0x02
-#define SHIFT_CURSOR        0x14
-#define SHIFT_DISPLAY       0x1C
+// LCD CONTROL CHARACTERS (only valid with RS == 0)
+typedef enum 
+{
+    LCD_CLEAR_DISPLAY =   (0x01u),
+    LCD_RETURN_HOME =     (0x02u),
+    LCD_SHIFT_CURSOR =    (0x14u),
+    LCD_SHIFT_DISPLAY =   (0x1Cu),
+} LCD_DisplayControlChars_t;
 
-void LCD_Init(void);
+/*!
+ * @brief Initialize the LCD display via SPI
+ *
+ * @param void
+ * @return void
+ */
+void LCD_Init( void );
 
 
